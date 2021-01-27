107aa0e541add4dd443788fa28df1a30a1089391
Rayssa23 <ray23@ufn.edu.br>
2021-01-27 15:26:29 -0300

    testeAcento

--- dev.c
+++ dev.c
@@ -8,10 +8,29 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
 
 void menuSobre(void);
+void telaInicial();
+void menuPrincipal(void);
+
 
 int main(void) {
+    printf("\n****** Verificando a localidade corrente ********\n\n");
+    printf ("Localidade corrente: %s\n", setlocale(LC_ALL,NULL) );
+    printf("Não é possível usar acentuação ou ç corretamente…\n\n");
+    printf("\n****** Alterando para a localidade do sistema ********\n\n");
+    //alterando para o padrão do sistema operacional
+    printf("A localidade corrente agora é %s \n",setlocale(LC_ALL,""));
+    printf("Agora não tem mais problema algum!\n");
+    printf("Já posso usar acentuação e também o caracter ç…\n\n\n");
+    system("pause");
+
+    //setlocale(LC_ALL, "Portuguese");
+    //printf("Utilizando caracteres e acentuação da língua portuguesa!\n\n");
+    //system("pause");
     menuSobre();
     telaInicial();
     menuPrincipal();
@@ -74,7 +93,7 @@ void menuPrincipal(){
     printf("|/////            3 - Investimentos                                        /////|\n");
     printf("|/////            4 - Contribuintes                                        /////|\n");
     printf("|/////            5 - Dependentes                                          /////|\n");
-    printf("|/////            6 - Relatorio                                            /////|\n");
+    printf("|/////            6 - Relatório                                            /////|\n");
     printf("|/////            7 - Informacoes sobre o sistema                          /////|\n");
     printf("|/////            0 - Sair                                                 /////|\n");
     printf("|///////////////////////////////////////////////////////////////////////////////|\n"); 
