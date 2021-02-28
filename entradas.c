2805eedd1c304c7d17ba7c6f6c815067e448e353
Rayssa23 <ray23@ufrn.edu.br>
2021-02-28 16:45:38 -0300

    subprogramaEntradas

---
+++ entradas.c
@@ -0,0 +1,93 @@
+//Modulo entradas
+//Subprograma
+
+
+void cadastroEntradas(void){
+    char nome[51];
+    char valor[12];
+    char tipo[20];
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");    
+        printf("|/////                  Modulo Cadastrar Entradas                          /////|\n");
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");
+        printf("|/////            Responsavel: ");
+        scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+	        getchar();
+        printf("|/////                                                                     /////|\n");    
+        printf("|/////            Valor(apenas numeros): ");
+        scanf("%[0-9]", valor);
+        getchar(); 
+        printf("|/////                                                                     /////|\n");           
+        printf("|/////            Tipo (Salario/Extras): ");
+        scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", tipo);
+	        getchar(); 
+        printf("|/////                                                                     /////|\n");   
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");
+        printf("\n");
+    printf("Responsavel: %s\n", nome);
+    printf("Valor: %s\n", valor);
+    printf("Tipo: %s\n", tipo);
+	    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    
+}
+
+void consultaEntradas(void){
+    char nome[51];
+    char valor[12];
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");    
+        printf("|/////                  Modulo Consultar Entradas                          /////|\n");
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");
+        printf("|/////            Informe o Responsavel(nome completo):                    /////|\n");
+        scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|/////            Informe o Valor:                                         /////|\n");
+        scanf("%[0-9]", valor);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n"); 
+        printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+        //delay(1);
+}
+
+void excluiEntradas(void){
+    char nome[51];
+    char valor[12];
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");    
+        printf("|/////                  Modulo Excluir Entradas                            /////|\n");
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");
+        printf("|/////            Informe o Responsavel(nome completo):                    /////|\n");
+        scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|/////            Informe o Valor:                                         /////|\n");
+        scanf("%[0-9]", valor);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n"); 
+        printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+        //delay(1);
+}
+
+void atualizaEntradas(void){
+    char nome[51];
+    char valor[12];
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");    
+        printf("|/////                  Modulo Aturalizar Entradas                         /////|\n");
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n");
+        printf("|/////            Informe o Responsavel(nome completo):                    /////|\n");
+        scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|/////            Informe o Valor:                                         /////|\n");
+        scanf("%[0-9]", valor);
+            getchar();
+        printf("|/////                                                                     /////|\n"); 
+        printf("|///////////////////////////////////////////////////////////////////////////////|\n"); 
+        printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+        getchar();
+        //delay(1);
+}
+
---
+++ entradas.h
@@ -0,0 +1,14 @@
+//Assinaturas das funções do Módulo Entradas
+
+
+//Cadastro
+void cadastroEntradas(void);
+
+//Consulta
+void consultaEntradas(void);
+
+//Excluir
+void excluiEntradas(void);
+
+//Atualizar
+void atualizaEntradas(void);
