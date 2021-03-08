fcaf40f3bb2c5fcb96b51180bedb9c58c32b0c75
Rayssa23 <ray23@ufrn.edu.br>
2021-03-08 15:00:22 -0300

    6/15 char

--- validacoes.c
+++ validacoes.c
@@ -15,7 +15,7 @@
 int validarNomes(char nome[]){
     int tam;
     char teste [1];
-    teste[1] = ('0','1','2','3','4','5','6','7','8','9','10');
+    teste[1] = ("0","1","2","3","4","5","6","7","8","9","10");
     tam = strlen (nome);
     if ((tam == 0 )|| (tam <= 5)){
         return 1;}     
